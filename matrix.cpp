953da0511153df5fcbf9a5ac55bb64ebcbf44668
HawkinsZhao <hawkinszhao@outlook.com>
2017-10-27 21:17:24 +0800

    [Init]

---
+++ matrix.cpp
@@ -0,0 +1,18 @@
+// 说明：
+// 请注意，本代码仅作为示例，严禁使用类似方法编写程序。
+// 任务说明：请编写 N=5，N=10，N=15 的情况
+// 任务开始日期：2017年10月27日
+// 任务截止日期：2017年11月10日
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "1 1 1 1 1" << endl;
+    cout << "1 2 2 2 1" << endl;
+    cout << "1 2 3 2 1" << endl;
+    cout << "1 2 2 2 1" << endl;
+    cout << "1 1 1 1 1" << endl;
+    return 0;
+}
+
+
